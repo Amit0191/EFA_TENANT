196f9731df04cc7ca1e0e3f864d30bda300a4f50
Amit Srivastava <amit.srivastava@tietoevry.com>
2023-02-16 10:44:17 +0100

    Added validate_all() function to validate list of Tenants.

--- validations.py
+++ validations.py
@@ -1,15 +1,25 @@
 from pydantic import ValidationError
 import json
 
-from pydantic_classes import EFABody
+from pydantic_classes import EFABody, EFAList
 
 
 def validate(response):
     if response.status_code == 201:
         try:
-            print('I am in')
-            efaBody = EFABody(**response.response)
-            return json.loads(efaBody.json())
+            efa_body = EFABody(**response.response)
+            return json.loads(efa_body.json())
+        except ValidationError as e:
+            return json.loads(e.json())
+    else:
+        return response.response[0].decode('UTF-8')
+
+
+def validate_all(response):
+    if response.status_code == 200:
+        try:
+            efa_list = EFAList(__root__= response.response)
+            return json.loads(efa_list.json())
         except ValidationError as e:
             return json.loads(e.json())
     else:
