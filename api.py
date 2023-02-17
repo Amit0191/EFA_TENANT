bcd080d8e95d0e869aa78e3e6b04c4a2fe43bfb8
Amit Srivastava <amit.srivastava@tietoevry.com>
2023-02-17 12:58:01 +0100

    Added doc string and dummy operations functionality

--- api.py
+++ api.py
@@ -1,28 +1,61 @@
-from connexion import NoContent
+'''
+    A module specified by connexion to implement endpoints according to OpenApi3 specs. 
+        Functions:
+            create_tenant():
+                Deals with the endpoint /create_tenant. Takes as an argument Tenant dictionary and returns a successful response if Tenant is created.
+                    Parameters:
+                            tenant (Dict): Dict of Tenant fields like name, num_of_vrf, description, etc.
+
+                    Returns:
+                            (Response): A successful response if tenant is created successfully.
+
+            get_tenants():
+                Deals with the endpoint /tenants. Returns a list of all Tenants available.
+                    Parameters:
+                            tenant (Dict): Dict of Tenant fields like name, num_of_vrf, description, etc.
+
+                    Returns:
+                            (Response): A successful response if tenant is created successfully.
+
+            get_tenant(name):
+                Deals with the endpoint /tenant/name. Returns a Tenant with tenant name = name.
+                    Parameters:
+                            name (str): String reflecting Tenant Name to be returned.
+
+                    Returns:
+                            (Response): Requested Tenant is returned if name matches If not, a 'Tenant Name doesnt exist is returned'.
+'''
+
 from mock import get_all_tenants, get_tenant_by_name
 from validations import validate, validate_all, validate_create
+from operations import add_vrf
+
+from typing import List, Dict
 
 
-def index():
+def index() -> str:
     
     return 'Hello'
 
 
-def create_tenant(tenant):
+def create_tenant(tenant: dict) -> str:
 
+    add_vrf = add_vrf(tenant, 50)
     val = validate_create(tenant)
     return val
 
 
-def get_tenants():
+def get_tenants() -> List:
     
     response = get_all_tenants()
     val = validate_all(response)
+    print(type(val))
     return val
 
 
-def get_tenant(name):
+def get_tenant(name: str) -> Dict:
     
     response = get_tenant_by_name(name)
     val = validate(response)
+    print(type(val))
     return val
