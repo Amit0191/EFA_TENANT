0e8c636f4f6480ad0e343df3a6e3d0e59c397f02
Amit Srivastava <amit.srivastava@tietoevry.com>
2023-02-17 14:31:10 +0100

    Added basic_auth function.

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import request
 import connexion
 
 
+
+
+PASSWD = {"admin": "secret", "foo": "bar"}
+
+def basic_auth(username, password):
+    if PASSWD.get(username) == password:
+        return {"sub": username}
+    # optional: raise exception for custom error response
+    return None
+
+
 app = connexion.App(__name__, specification_dir='oapi/')
 app.add_api('efa_tenant.yaml')
 
